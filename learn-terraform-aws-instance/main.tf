1a3b6c116b056af7daf2c2a584479d76eaba7980
kokomal98 <al.ivanovv98@gmail.com>
2023-03-26 20:03:00 +0300

    Change Infrastructure with output

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d55b7853706b84fa62a7f7e2d4e22fa11724f9f28bb23f842d24855f91d6070
+oid sha256:fc12cbc583d642069095b1e688418ab7373dc514d25e5facde80ebd97a0faef7
 size 381
