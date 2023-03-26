5a20a21769eb12d25da3885bd3f9fbd3a465c606
kokomal98 <al.ivanovv98@gmail.com>
2023-03-26 20:13:20 +0300

    Input variables defined

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc12cbc583d642069095b1e688418ab7373dc514d25e5facde80ebd97a0faef7
-size 381
+oid sha256:ab9fc69840280c8aadd8d592b6d5935e40c728dff8c5ccb8bab7abe6e8f14a1a
+size 372
---
+++ learn-terraform-aws-instance/variables.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:153dfcd4e9a4163cea085efaac2c168ecc32970602e761bb403ebba2ed8a9d78
+size 161
